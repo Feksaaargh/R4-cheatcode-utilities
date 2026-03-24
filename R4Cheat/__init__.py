d060623bd60c7e5b5d58077361a5e1edb2642005
Feksaaargh <158505890+Feksaaargh@users.noreply.github.com>
2026-03-24 16:43:19 -0500

    initial commit

---
+++ R4Cheat/__init__.py
@@ -0,0 +1,397 @@
+from math import ceil
+
+# ENTIRE IMPLEMENTATION IS BASED ON ARTICLE BY Nate Reprogle
+# From https://medium.com/@natereprogle/reverse-engineering-a-long-lost-file-format-usrcheat-dat-2c15fefe2f63
+
+
+# Reads a null terminated string that is padded to 4 bytes
+def read_4byte_padded_string(file_handle) -> bytes:
+    file_pos_start = file_handle.tell()
+    res = bytes()
+    while True:
+        chunk = file_handle.read(4)
+        if len(chunk) < 4:
+            raise ValueError(f"Got EOF when reading a 4 byte padded string (started at {file_pos_start})")
+        for i in range(4):
+            if chunk[i] == 0:
+                res += chunk[0:i]
+                return res
+        res += chunk
+
+
+def make_4byte_padded_string(inp: str, encoding: str) -> bytes:
+    retval: bytes = inp.encode(encoding)
+    retval += b'\0' * (4 - (len(retval) % 4))
+    return retval
+
+
+# Reads two null terminated strings that are all together padded to 4 bytes
+def read_name_comment_pair(file_handle) -> tuple[bytes, bytes]:
+    file_pos_start = file_handle.tell()
+    name: bytes = bytes()
+    comment: bytes = bytes()
+    reading_name: bool = True
+    while True:
+        chunk = file_handle.read(4)
+        if len(chunk) < 4:
+            raise ValueError(f"Got EOF when reading a name/comment pair (started at {file_pos_start})")
+        for i in range(4):
+            if chunk[i] == 0:
+                if reading_name:
+                    reading_name = False
+                    continue
+                else:
+                    return (name, comment)
+            if reading_name:
+                name += chunk[i:i+1]
+            else:
+                comment += chunk[i:i+1]
+
+
+def make_name_comment_pair(name: str, comment: str, encoding: str) -> bytes:
+    retval: bytes = name.encode(encoding) + b'\0' + comment.encode(encoding)
+    retval += b'\0' * (4 - (len(retval) % 4))
+    return retval
+
+
+class LoadingOptions:
+    def __init__(self, encoding: str, allow_automatic_fixes: bool):
+        self.encoding = encoding
+        self.allow_automatic_fixes = allow_automatic_fixes
+
+
+class AddressBookEntry:
+    def __init__(self):
+        self.game_ID: bytes = b'\0\0\0\0'
+        self.checksum: bytes = b'\0\0\0\0'
+        self.offset: int = 0
+        self.reserved: bytes = b'\0\0\0\0'
+
+    def load(self, file_handle) -> "AddressBookEntry":
+        rawbytes = file_handle.read(16)
+        self.game_ID: bytes = rawbytes[0:4]
+        self.checksum: bytes = rawbytes[7:3:-1]
+        self.offset: int = int.from_bytes(rawbytes[8:12], "little")
+        self.reserved: bytes = rawbytes[12:16]
+        if self.reserved != b'\0\0\0\0':
+            print("WARNING: Supposed reserved section in address book entry is NOT blank! Bad file?")
+        return self
+
+    def is_blank(self) -> bool:
+        return (self.game_ID == b'\0\0\0\0') and \
+               (self.checksum == b'\0\0\0\0') and \
+               (self.offset == 0) and \
+               (self.reserved == b'\0\0\0\0')
+
+    def encode(self, options: LoadingOptions) -> bytes:
+        if len(self.game_ID) != 4 or len(self.checksum) != 4:
+            raise ValueError("Failed to encode AddressBookEntry: length of values were incorrect")
+        return self.game_ID + self.checksum + self.offset.to_bytes(4, "little") + self.reserved
+
+    def __str__(self):
+        return f"AddressBookEntry(game_ID={self.game_ID}, checksum={self.checksum}, offset={self.offset}, reserved={self.reserved})"
+
+
+class CheatEntry:
+    def __init__(self):
+        self.name: str = ""
+        self.comment: str = ""
+        self.cheat: bytes = b''
+        self.enabled: bool = False
+
+    def load(self, file_handle, options: LoadingOptions, size: int, enabled: bool) -> "CheatEntry":
+        name_and_comment = read_name_comment_pair(file_handle)
+        name_and_comment_len = ceil((len(name_and_comment[0]) + 1 + len(name_and_comment[1])+1) / 4) * 4
+        self.name: str = name_and_comment[0].decode(options.encoding)
+        self.comment: str = name_and_comment[1].decode(options.encoding)
+        cheat_length = int.from_bytes(file_handle.read(4), "little")
+        self.cheat: list[int] = [int.from_bytes(file_handle.read(4), "little") for _ in range(cheat_length)]
+        self.enabled: bool = enabled
+        intended_size = name_and_comment_len + (cheat_length * 4) + 4
+        if intended_size != size * 4:
+            print(f"WARNING: Cheat entry inconsistent; size did not reflect content size accurately (was {intended_size}, thought it would be {size * 4})")
+        return self
+
+    # This includes the length and flags in front of the cheat entry
+    def encode(self, options: LoadingOptions) -> bytes:
+        # skipping length for now
+        retval: bytes = b'\x00\x01' if self.enabled else b'\x00\x00'
+        retval += make_name_comment_pair(self.name, self.comment, options.encoding)
+        retval += (len(self.cheat) // 4).to_bytes(4, "little")
+        retval += self.cheat
+        # add length here
+        retval = (len(retval) // 4).to_bytes(2, "little") + retval
+        return retval
+
+    def __str__(self):
+        retval = f"CHEAT ENTRY | Name = \"{self.name}\" | Comment = \"{self.comment}\" | Enabled = {self.enabled}\n"
+        encoded_cheat = [hex(chunk).replace("0x", "") for chunk in self.cheat]
+        encoded_cheat = ' '.join([f"{i:0>8}" for i in encoded_cheat])
+        retval += f"Cheat: \"{encoded_cheat}\""
+        return retval
+
+
+class CheatFolder:
+    def __init__(self):
+        self.name: str = ""
+        self.comment: str = ""
+        self.is_onehot_button: bool = False
+        self.owned_cheats: list[CheatEntry] = []
+        self._allow_automatic_fixes = True
+
+    def load(self, file_handle, options: LoadingOptions, is_onehot_button: bool) -> "CheatFolder":
+        name_and_comment = read_name_comment_pair(file_handle)
+        self.name: str = name_and_comment[0].decode(options.encoding)
+        self.comment: str = name_and_comment[1].decode(options.encoding)
+        self.is_onehot_button: bool = is_onehot_button
+        self.owned_cheats: list[CheatEntry] = []
+        self._allow_automatic_fixes = options.allow_automatic_fixes
+        return self
+
+    def append(self, entry: CheatEntry):
+        self.owned_cheats.append(entry)
+
+    # If it's a onehot button, checks and corrects if zero or more than one entries within are enabled.
+    # If zero entries are enabled, enables the first one.
+    # If more than one entry is enabled, disables all but the first enabled one.
+    # Prints a warning if anything was corrected.
+    # If it's not a onehot button, this does nothing.
+    def check_consistency(self):
+        if (not self.is_onehot_button) or (not self._allow_automatic_fixes):
+            return
+        first_enabled_cheat_index = -1
+        num_enabled_cheats = 0
+        for idx, cheat in enumerate(self.owned_cheats):
+            if not cheat.enabled: continue
+            num_enabled_cheats += 1
+            if first_enabled_cheat_index != -1:
+                cheat.enabled = False
+            else:
+                first_enabled_cheat_index = idx
+        if num_enabled_cheats == 0:
+            self.owned_cheats[0].enabled = True
+            print(f"Folder with onehot button ({self.name}) had zero cheats enabled. Enabled the first one ({self.owned_cheats[0].name})")
+        elif num_enabled_cheats > 1:
+            print(f"Folder with onehot button ({self.name}) had too many cheats enabled. Disabled all but the first enabled entry.")
+
+    def encode(self, options: LoadingOptions) -> bytes:
+        retval: bytes = len(self.owned_cheats).to_bytes(2, "little")
+        retval += b'\x00\x11' if self.is_onehot_button else b'\x00\x10'
+        retval += make_name_comment_pair(self.name, self.comment, options.encoding)
+        for cheat in self.owned_cheats:
+            retval += cheat.encode(options)
+        return retval
+
+    # returns number of owned cheats (EXCLUDING SELF!!)
+    def __len__(self) -> int:
+        return len(self.owned_cheats)
+
+    def __str__(self):
+        retval = f"CHEAT FOLDER | Name = \"{self.name}\" | Comment = \"{self.comment}\" | Is onehot button = {self.is_onehot_button}\n"
+        retval += f"Owned cheats: ["
+        stringified_contents = ""
+        for item in self.owned_cheats:
+            stringified_contents += "\n"
+            stringified_contents += str(item)
+        stringified_contents = stringified_contents.replace("\n", "\n\t")
+        retval += stringified_contents + "\n]"
+        return retval
+
+
+class GameEntry:
+    def __init__(self):
+        self.name: str = ""
+        self.game_ID: str = "AAAA"
+        self.checksum: bytes = b'\0\0\0\0'
+        self.enabled: bool = True
+        self.master_code: list[int] = [0] * 8
+        self.contents: list[CheatFolder | CheatEntry] = []
+
+    def load(self, file_handle, options: LoadingOptions, game_ID: bytes, checksum: bytes) -> "GameEntry":
+        self.name: str = read_4byte_padded_string(file_handle).decode(options.encoding)
+        self.game_ID: str = game_ID.decode("ascii")
+        self.checksum: bytes = checksum
+        n_entries: int = int.from_bytes(file_handle.read(2), "little")
+        self.enabled: bool = file_handle.read(2) == b'\x00\xF0'
+        master_code_unflipped: bytes = file_handle.read(32)
+        self.master_code: list[int] = [int.from_bytes(master_code_unflipped[i:i+4], "little") for i in range(0, 32, 4)]
+        self.read_contents(file_handle, options, n_entries)
+        return self
+
+    def read_contents(self, file_handle, options: LoadingOptions, num_entries: int):
+        num_cheats_owed_to_folder = 0
+        for _ in range(num_entries):
+            value: int = int.from_bytes(file_handle.read(2), "little")
+            entry_type_bytes: bytes = file_handle.read(2)
+            flag: bool = bool(entry_type_bytes[1] & 0x01)
+            is_cheat = not bool(entry_type_bytes[1] & 0x10)
+            # check if any other bits in the entry_type_bytes are set
+            if entry_type_bytes[0] != 0 or entry_type_bytes[1] & 0xEE != 0:
+                raise ValueError("Unknown game cheat entry flags set!")
+            if is_cheat:
+                # is a cheat
+                cheat = CheatEntry().load(file_handle, options, size=value, enabled=flag)
+                if num_cheats_owed_to_folder == 0:
+                    self.contents.append(cheat)
+                else:
+                    self.contents[-1].append(cheat)
+                    num_cheats_owed_to_folder -= 1
+                    # Check if cheats inside the folder are all good
+                    if num_cheats_owed_to_folder == 0:
+                        self.contents[-1].check_consistency()
+            else:
+                # is a folder
+                if num_cheats_owed_to_folder != 0:
+                    raise ValueError("Corrupt structure: Folder tried to contain another folder, this is impossible")
+                self.contents.append(CheatFolder().load(file_handle, options, is_onehot_button=flag))
+                num_cheats_owed_to_folder = value
+
+    # returns number of cheats inside this (including folders and items in folders)
+    def __len__(self) -> int:
+        retval: int = 0
+        for entry in self.contents:
+            if isinstance(entry, CheatEntry):
+                retval += 1
+            else:
+                retval += 1 + len(entry)
+        return retval
+
+    def encode(self, options: LoadingOptions) -> bytes:
+        retval: bytes = make_4byte_padded_string(self.name, options.encoding)
+        retval += len(self).to_bytes(2, "little")
+        retval += b'\x00\xF0' if self.enabled else b'\x00\x00'
+        retval += b''.join([i.to_bytes(4, "little") for i in self.master_code])
+        retval += b''.join([entry.encode(options) for entry in self.contents])
+        return retval
+
+    def __str__(self):
+        retval = f"GAME ENTRY | Name = \"{self.name}\" | Game ID = {self.game_ID} | Checksum = {self.checksum}\n"
+        master_code_repr = [i.to_bytes(4, "big").hex() for i in self.master_code]
+        retval += f"Master code enabled = {self.enabled} | Master code = {master_code_repr}"
+        stringified_contents = ""
+        for item in self.contents:
+            stringified_contents += "\n"
+            stringified_contents += str(item)
+        stringified_contents = stringified_contents.replace("\n", "\n\t")
+        retval += stringified_contents
+        return retval
+
+
+class R4CheatFile:
+    def __init__(self, allow_automatic_fixes: bool):
+        self.name: str = ""
+        self.encoding: str = "utf-8"
+        self.game_entries: list[GameEntry] = []
+        self.enabled: bool = True
+        # Address book does not need to be serialized and is only stored in case the user wants to
+        #  write back the same file that was read in
+        self._address_book: list[AddressBookEntry] = []
+        self._allow_automatic_fixes: bool = allow_automatic_fixes
+
+    # file_handle must be in byte mode
+    def load(self, file_handle) -> "R4CheatFile":
+        # read header
+        self._load_header(file_handle.read(0x100))
+        # read address book
+        address_book: list[AddressBookEntry] = []
+        while True:
+            working_entry = AddressBookEntry().load(file_handle)
+            if working_entry.is_blank():
+                break
+            address_book.append(working_entry)
+        # parse address book entries
+        for address_book_entry in address_book:
+            file_handle.seek(address_book_entry.offset)
+            # since a game entry is of an unknown size, handing off reading responsibility to the GameEntry class
+            self.game_entries.append(GameEntry().load(
+                file_handle,
+                LoadingOptions(self.encoding, self._allow_automatic_fixes),
+                address_book_entry.game_ID,
+                address_book_entry.checksum))
+        return self
+
+    # @param header The first 256 bytes from the top of the file
+    def _load_header(self, header: bytes):
+        if header[0x0:0x0C] != b'R4 CheatCode':
+            raise ValueError("File does not appear to be a valid R4 Cheat Code file! (incorrect signature)")
+
+        if header[0x0C:0x10] != b'\x00\x01\x00\x00':
+            print("WARNING: Header has unknown version (?) (bytes 0x0C-0x0F) This might be an unsupported R4 cheat code file!")
+
+        encoding_bytes = header[0x4C:0x50]
+        match encoding_bytes:
+            case b'\xD5\x53\x41\x59':
+                self.encoding = "gbk"
+            case b'\xF5\x53\x41\x59':
+                self.encoding = "big5"
+            case b'\x75\x53\x41\x59':
+                self.encoding = "shift_jis"
+            case b'\x55\x73\x41\x59':
+                self.encoding = "utf-8"
+            case _:
+                raise ValueError(f"Error loading file: Unknown encoding: {encoding_bytes.hex()}")
+
+        name_end = 0x10
+        for i in range(0x10, 0x4C):
+            if header[i] == 0:
+                name_end = i
+                break
+        self.name = header[0x10:name_end].decode(self.encoding)
+        self.enabled = header[0x50] != 0
+
+        for i in range(0x51, 0x100):
+            if header[i] != 0:
+                print(f"WARNING: Header byte {hex(i)} was nonzero when it should have been zero. This might be an unsupported R4 cheat code file!")
+
+    # Turn the object back into a valid usrcheat file
+    # file_handle must be in byte mode
+    def write(self, file_handle):
+        # create header
+        file_handle.write(b'R4 CheatCode\x00\x01\x00\x00')
+        file_handle.write(self.name.encode(self.encoding))
+        file_handle.write(b'\x00' * (0x4C - len(file_handle.tell())))
+        match self.encoding:
+            case "gbk":
+                file_handle.write(b'\xD5\x53\x41\x59')
+            case "big5":
+                file_handle.write(b'\xF5\x53\x41\x59')
+            case "shift_jis":
+                file_handle.write(b'\x75\x53\x41\x59')
+            case "utf-8":
+                file_handle.write(b'\x55\x73\x41\x59')
+            case _:
+                raise ValueError("Don't poke into the object like that.")
+        file_handle.write(b'\x01' if self.enabled else b'\x00')
+        file_handle.write(b'\x00' * (0x100 - file_handle.tell()))
+
+        # encode game entries (need to do this before creating address book)
+        encoded_games: list[bytes] = []
+        for game in self.game_entries:
+            encoded_games.append(game.encode(LoadingOptions(self.encoding, self._allow_automatic_fixes)))
+
+        # starting position is header + length of entire address book
+        game_offset = 0x100 + ((len(encoded_games)+1) * 32)
+        for game_idx in range(len(encoded_games)):
+            entry = AddressBookEntry()
+            entry.game_ID = self.game_entries[game_idx].game_ID.encode("ascii")
+            entry.checksum = self.game_entries[game_idx].checksum
+            entry.offset = game_offset
+            game_offset += len(encoded_games[game_idx])
+            file_handle.write(entry.encode(LoadingOptions(self.encoding, self._allow_automatic_fixes)))
+
+        # can now add game entries to result
+        for game in encoded_games:
+            file_handle.write(game)
+
+    def __str__(self):
+        retval = "=== R4 Cheat File ===\n"
+        retval += f"Encoding: {self.encoding}\n"
+        retval += f"Enabled: {self.enabled}\n"
+        retval += "Game entries:"
+        stringified_contents = ""
+        for game in self.game_entries:
+            stringified_contents += "\n"
+            stringified_contents += str(game)
+        stringified_contents = stringified_contents.replace("\n", "\n\t")
+        retval += stringified_contents
+        return retval
---
+++ usrcheatjsonifier.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+from R4Cheat import R4CheatFile, GameEntry, CheatEntry, CheatFolder
+import json
+import argparse
+
+
+# Convert bytes to hexadecimal values and split into bytes_per_split size chunks
+# Does NOT produce strange things like 0x or anything, just has 0-F and the delimiter
+def hexify_and_split(inp: bytes, bytes_per_split: int, delimiter: str = " "):
+    hexified = inp.hex().upper()
+    chars_per_split = bytes_per_split * 2
+    num_segments = len(hexified) // chars_per_split
+    return delimiter.join([hexified[chars_per_split*i:min(chars_per_split*(i+1), len(hexified))] for i in range(num_segments)])
+
+
+def dictionaryify_cheat_entry(entry: CheatEntry):
+    # encoded_cheat = [hex(chunk).replace("0x", "") for chunk in entry.cheat]
+    # encoded_cheat = ' '.join([f"{i:0>8}" for i in encoded_cheat])
+    return {
+        "type": "cheat",
+        "name": entry.name,
+        "comment": entry.comment,
+        "enabled": entry.enabled,
+        "code": hexify_and_split(b''.join(i.to_bytes(4, "big") for i in entry.cheat), 4)
+    }
+
+
+def dedictionaryify_cheat_entry(entry: dict) -> CheatEntry:
+    assert entry["type"] == "cheat"
+    retval = CheatEntry()
+
+
+def dictionaryify_cheat_folder(folder: CheatFolder):
+    return {
+        "type": "folder",
+        "name": folder.name,
+        "comment": folder.comment,
+        "onehot": folder.is_onehot_button,
+        "entries": [dictionaryify_cheat_entry(entry) for entry in folder.owned_cheats]
+    }
+
+
+def dedictionarify_cheat_folder(entry: dict) -> CheatFolder:
+    pass
+
+
+def dictionaryify_game_entry(game: GameEntry):
+    dictionaryified_cheats = []
+    for entry in game.contents:
+        if isinstance(entry, CheatFolder):
+            dictionaryified_cheats.append(dictionaryify_cheat_folder(entry))
+        elif isinstance(entry, CheatEntry):
+            dictionaryified_cheats.append(dictionaryify_cheat_entry(entry))
+        else:
+            print("Something very bad has happened. (game entry had strange contents)")
+    return {
+        "name": game.name,
+        "gameID": game.game_ID,
+        "checksum": game.checksum.hex(),
+        "enabled": game.enabled,
+        "masterCode": hexify_and_split(b''.join(i.to_bytes(4, "big") for i in game.master_code), 4),
+        "cheats": dictionaryified_cheats
+    }
+
+def dedictionarify_game_entry(entry: dict) -> GameEntry:
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="usercheatjsonifier",
+        description="A program which converts usrcheat.dat files to plain JSON and back",
+    )
+
+    # optional arguments
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s v0.0')
+    parser.add_argument('-d', '--decode', help="Decode a JSON file instead of encoding it", action='store_true')
+    parser.add_argument('--overwrite', help="Force overwriting file at destination", action='store_true')
+    parser.add_argument('-n', '--no-fix', help="Do not automatically fix detected errors (leave onehot buttons as-is)", action='store_true')
+
+    # positional arguments
+    parser.add_argument('source', help="Source file")
+    parser.add_argument('dest', help="Output file")
+
+    args = parser.parse_args()
+
+    # ====================================
+
+    if args.decode:
+        # TODO: Implement
+        print("NYI")
+    else:
+        # encoding file
+        usrcheat: R4CheatFile = R4CheatFile(not args.no_fix)
+        # file.load_file("/home/feksa/Downloads/usrcheat.dat")
+        with open(args.source, "rb") as file:
+            usrcheat.load(file)
+
+        output = {
+            "name": usrcheat.name,
+            "encoding": usrcheat.encoding,
+            "enabled": usrcheat.enabled,
+            "games": [dictionaryify_game_entry(game) for game in usrcheat.game_entries]
+        }
+
+        with open(args.dest, "wt" if args.overwrite else "xt") as dest:
+            json.dump(output, dest, indent=2)
+
+        print("Done encoding")
+
+
+if __name__ == "__main__":
+    main()
